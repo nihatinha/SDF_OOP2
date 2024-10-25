fee411f6928eada4057797eacd378d243223193d
nihatinha <nqarayev5@std.beu.edu.az>
2024-10-25 14:40:12 +0400

    First

---
+++ src/main/java/com/example/SDF_OOP2/SdfOop2Application.java
@@ -0,0 +1,13 @@
+package com.example.SDF_OOP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdfOop2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdfOop2Application.class, args);
+	}
+
+}
